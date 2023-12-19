2f1dc9e82241e4b9019c4005f517c974a55c328d
devadrsilva <adriano.mssilva1@gmail.com>
2023-12-19 10:24:44 -0300

    frist commit

---
+++ spec/system/user_see_home_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe "Usuário chega na página home" do
+  it "com sucesso" do
+
+    # Arrange
+
+    # Act
+    visit root_path
+
+    # Assert
+
+    expect(page).to have_content 'Cheia de Charme'
+
+  end
+
+  it "usuário vê lista de clientes" do
+
+    # Arrange
+    customer1 = Customer.create!(name: "Adriano", last_name: "Silva", birthday: '08/02/1991', street: 'rua das mangueiras',
+                                  district: 'Brotas', number: '40', complement: 'apto 203', city: 'Salvador', state: 'Bahia')
+    customer2 = Customer.create!(name: "Bianca", last_name: "Sedro", birthday: '31/10/1999', street: 'rua das margaridas',
+                                  district: 'Barra', number: '200', complement: 'apto 500', city: 'Salvador', state: 'Bahia')
+    customer3 = Customer.create!(name: "João", last_name: "Santos", birthday: '20/10/2023', street: 'rua das palmeiras',
+                                  district: 'Limoeiro', number: '20', complement: 'apto 400', city: 'Salvador', state: 'Bahia')
+
+    # Act
+    visit root_path
+
+    # Assert
+
+    expect(page).to have_content 'Adriano Silva'
+    expect(page).to have_content 'Bianca Sedro'
+    expect(page).to have_content 'João Santos'
+    expect(page).to have_content 'Aniversário: 08/02/1991'
+    expect(page).to have_content 'Aniversário: 31/10/1999'
+    expect(page).to have_content 'Aniversário: 20/10/2023'
+
+  end
+
+
+end
