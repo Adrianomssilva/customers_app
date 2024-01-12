15d58bd09c1eb60d52ea97a9d9fc638b2578267a
Adrianomssilva <adriano.mssilva1@gmail.com>
2024-01-11 21:49:06 -0300

    WIP soft delete, rota delete não está aparecendo

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :customers, only: [:show, :index, :new, :create, :edit, :update]
+  resources :customers, only: [:show, :index, :new, :create, :edit, :update, :delete]
 end
