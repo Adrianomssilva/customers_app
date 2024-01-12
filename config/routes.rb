2adf465a039f9e710f14be574b5cbf73edf0c36e
devadrsilva <adriano.mssilva1@gmail.com>
2024-01-12 17:17:07 -0300

    WIP SOFT DELETE, Ajuste do button_to para que a class css e as altreações de metodo e url funcionem!

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :customers, only: [:show, :index, :new, :create, :edit, :update, :delete]
+  resources :customers, only: [:show, :index, :new, :create, :edit, :update] do
+    patch 'delete', on: :member
+  end
 end
